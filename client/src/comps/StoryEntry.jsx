979e915a0a8997ecbb779b7b2d8b7243681d3f03
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-09-27 15:18:26 -0700

    fixes proptypes. adds keys.js

--- client/src/comps/StoryEntry.jsx
+++ client/src/comps/StoryEntry.jsx
@@ -32,8 +32,6 @@ function StoryEntry(props) {
     props.updateNomination(ID, nomCount);
   };
 
-  console.log(props);
-
   return (
     <div className="story-entry" data-id={props.story._id}>
       <img src={props.story.photo_url} alt="user post" />
@@ -57,39 +55,38 @@ function StoryEntry(props) {
 }
 
 StoryEntry.propTypes = {
-  fetchStories: PropTypes.func.isRequired,
+  story: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    city: PropTypes.string.isRequired,
+    county: PropTypes.string.isRequired,
+    _id: PropTypes.string.isRequired,
+    nominations: PropTypes.number.isRequired,
+    photo_url: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    upvotes: PropTypes.number.isRequired,
+    zipcode: PropTypes.number.isRequired,
+  }).isRequired,
   updateVote: PropTypes.func.isRequired,
   updateNomination: PropTypes.func.isRequired,
   isFetching: PropTypes.bool.isRequired,
   didInvalidate: PropTypes.bool.isRequired,
-  filterBy: PropTypes.shape({
-    topStories: PropTypes.shape({
-      items: PropTypes.arrayOf(PropTypes.shape({
-        author: PropTypes.string.isRequired,
-        city: PropTypes.string.isRequired,
-        county: PropTypes.string.isRequired,
-        _id: PropTypes.string.isRequired,
-        nominations: PropTypes.number.isRequired,
-        photo_url: PropTypes.string.isRequired,
-        state: PropTypes.string.isRequired,
-        tag: PropTypes.string.isRequired,
-        text: PropTypes.string.isRequired,
-        title: PropTypes.string.isRequired,
-        upvotes: PropTypes.number.isRequired,
-        zipcode: PropTypes.number.isRequired,
-      })),
-    }),
-  }).isRequired,
-  geoScope: PropTypes.string,
 };
 
-const mapStateToProps = state => (state.stories);
+const mapStateToProps = state => state.stories;
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({
-    updateVote: actions.updateVote,
-    updateNomination: actions.updateNomination,
-  }, dispatch);
-};
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      updateVote: actions.updateVote,
+      updateNomination: actions.updateNomination,
+    },
+    dispatch,
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(StoryEntry);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(StoryEntry);
--- server/server.js
+++ server/server.js
@@ -4,69 +4,61 @@ const { MongoClient } = require('mongodb');
 const DB = require('../database/mongo.js');
 const bodyParser = require('body-parser');
 
-
 const app = express();
+
+const dbURI = require('../config/keys').mongoURI;
 
 const dbHost = process.env.DATABASE_HOST || 'localhost';
 const port = process.env.PORT || 8000;
 const userZipcode = process.env.ZIPCODE || 94121;
 
 app.use(bodyParser.json());
-app.use('/scope/hood/:zipcode', express.static(path.join(__dirname, '../client/dist')));
+app.use(
+  '/scope/hood/:zipcode',
+  express.static(path.join(__dirname, '../client/dist'))
+);
 
 app.get('/', (req, res) => {
   res.status(302).redirect(`/scope/hood/${userZipcode}`);
 });
 
+MongoClient.connect(
+  `mongodb://${dbHost}/`,
+  { useNewUrlParser: true },
+  (err, client) => {
+    if (err) {
+      throw err;
+    } else {
+      const db = client.db('communityNews');
+      const collection = db.collection('articles');
 
-MongoClient.connect(`mongodb://${dbHost}/`, { useNewUrlParser: true }, (err, client) => {
-  if (err) {
-    throw err;
-  } else {
-    const db = client.db('communityNews');
-    const collection = db.collection('articles');
-    
-    app.get('/zipcode', async (req, res) => {
-      console.log('HERE');
-      let { ID } = req.query;
-      ID = Number(ID);
-      const data = await collection.find({ zipcode: ID }).sort({ upvotes: -1 }).toArray();
-      res.send(data);
-    });
-    
-    app.get('/county', async (req, res) => {
-      let { county } = req.query;
-      const data = await collection.find({ county }).sort({ upvotes: -1 }).toArray();
-      res.send(data);
-    });
+      app.get('/zipcode', async (req, res) => {
+        let { ID } = req.query;
+        ID = Number(ID);
+        const data = await collection
+          .find({ zipcode: ID })
+          .sort({ upvotes: -1 })
+          .toArray();
+        res.send(data);
+      });
 
-    app.get('/city', async (req, res) => {
-      let { city } = req.query;
-      const data = await collection.find({ city }).sort({ upvotes: -1 }).toArray();
-      res.send(data);
-    });
+      app.post('/upVote', async (req, res) => {
+        const { voteCount, ID } = req.body.data;
+        await DB.updateVote(ID, collection, voteCount);
+        res.end();
+      });
 
-    app.get('/state', async (req, res) => {
-      let { state } = req.query;
-      const data = await collection.find({ state }).sort({ upvotes: -1 }).toArray();
-      res.send(data);
-    });
-
-    app.post('/upVote', async (req, res) => {
-      const { voteCount, ID } = req.body.data;
-      // console.log('votecount:', voteCount, 'ID:', ID);
-      await DB.updateVote(ID, collection, voteCount);
-      res.end();
-    });
-
-    app.post('/upNom', async (req, res) => {
-      const { nomCount, ID } = req.body.data;
-      console.log('votecount:', nomCount, 'ID:', ID);
-      await DB.updateNomination(ID, collection, nomCount);
-      res.end();
-    });
+      app.post('/upNom', async (req, res) => {
+        const { nomCount, ID } = req.body.data;
+        await DB.updateNomination(ID, collection, nomCount);
+        res.end();
+      });
+    }
   }
-});
+);
 
-
-app.listen(port, () => console.log(`Server's good to go on port ${port}... and may I say... you have got it going on today!`));
+app.listen(port, () =>
+  console.log(
+    `Server's good to go on port ${port}... and may I say... you have got it going on today!`
+  )
+);
