3c8af0052d49c323281bf687db32d959e9ab035a
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-10-16 12:21:17 -0700

    creates production build store and development store.  commented out

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -7,14 +7,24 @@ const initialState = {};
 
 const middleware = [thunk];
 
+// FOR DEVELOPMENT
+/* eslint-disable no-underscore-dangle */
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//   ),
+// );
+/* eslint-enable */
+
+// FOR PRODUCTION
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  compose(applyMiddleware(...middleware)),
 );
 /* eslint-enable */
 
