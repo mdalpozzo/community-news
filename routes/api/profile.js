8d9b6863927fc4b5ebd422bbcaecacc047033730
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-10-18 10:36:19 -0700

    adds delete route for profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -183,7 +183,7 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
 // @route   POST api/profile/education
 // @desc    Add education to profile
 // @access  Private
-router.post('/experience', passport.authenticate('jwt', { session: false }), (req, res) => {
+router.post('/education', passport.authenticate('jwt', { session: false }), (req, res) => {
   const { errors, isValid } = validateEducationInput(req.body);
 
   // Check Validation
@@ -208,6 +208,25 @@ router.post('/experience', passport.authenticate('jwt', { session: false }), (re
 
     profile.save().then(profile => res.json(profile));
   });
+});
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.post('/experience/:exp_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Profile.findOne({ user: req.user.id }).then((profile) => {
+    // GET remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    // Splice out of array
+    profile.experience.splice(removeIndex, 1);
+
+    // Save
+    profile.save().then(profile => res.json());
+  })
+    .catch(err => res.status(404).json(err));
 });
 
 module.exports = router;
