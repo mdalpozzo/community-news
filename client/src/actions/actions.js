b560a7157d6e83b6e7e35d955e075c870a743083
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2018-10-25 11:44:09 -0700

    converts to production mode in actions.js, store

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 import * as types from './actionTypes';
 
 // DEVELOPMENT
-const apiHost = 'http://localhost:8000/';
+// const apiHost = 'http://localhost:8000/';
 // PRODUCTION WITH HEROKU
-// const apiHost = 'https://communitynews88.herokuapp.com/';
+const apiHost = 'https://communitynews88.herokuapp.com/';
 
 function url(endpoint) {
   return apiHost + endpoint;
--- server.js
+++ server.js
@@ -21,8 +21,8 @@ app.use(bodyParser.json());
 
 const dbURL = process.env.DB_HOST || 'mongodb://localhost/communityNews';
 const port = process.env.PORT || 8000;
-const userZipcode = process.env.ZIPCODE || 94121;
-const apiHost = process.env.API_HOST || 'http://localhost:8000/';
+// const userZipcode = process.env.ZIPCODE || 94121;
+// const apiHost = process.env.API_HOST || 'http://localhost:8000/';
 
 // Connect to MongoDB
 mongoose
